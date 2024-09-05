409098260c09abddaf8b716408e1f99f0f60e020
LouisTenebrae <8975@holbertonstudents.com>
2024-09-05 14:10:28 -0400

    Updating the anims and other testing stuff

--- dungeons-of-stone/Script/Player/knight_player.gd
+++ dungeons-of-stone/Script/Player/knight_player.gd
@@ -4,6 +4,11 @@ var input
 @export var speed = 100.0
 @export var gravity = 10
 
+# Var for jump
+var jump_count = 0
+@export var max_jump = 2
+@export var jump_force = 500
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,27 +23,43 @@ func movement(delta):
 	input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if input != 0:
-		$Spriteidle.hide()
-		$SpriteRun.show()
+		$AnimSprite.play("run")
 		if input > 0:
-			$SpriteRun.flip_h = false
-			$AnimationPlayer.play("run")
+			$AnimSprite.flip_h = false
 			velocity.x += speed * delta
 			velocity.x = clamp(speed, 100.0, speed)
-		
 			
 		if input < 0:
-			$AnimationPlayer.play("run")
-			$SpriteRun.flip_h = true
+			$AnimSprite.flip_h = true
 			velocity.x -= speed * delta
 			velocity.x = clamp(-speed, 100.0, -speed)
 
 	if input == 0:
-		$SpriteRun.hide()
-		$Spriteidle.show()
 		velocity.x = 0
-		$AnimationPlayer.play("idle")
-	
+		$AnimSprite.play("idle")
+
+# Jump Code
+	if is_on_floor():
+		jump_count = 0
+	if !is_on_floor():
+		if velocity.y < 0:
+			$AnimSprite.play("jump")
+			
+		if velocity.y > 0:
+			$AnimSprite.play("fall")
+		
+	if Input.is_action_pressed("ui_accept") && is_on_floor() && jump_count < max_jump:
+		jump_count += 1
+		velocity.y -= jump_force
+		velocity.x = input
+	if !is_on_floor() && Input.is_action_just_pressed("ui_accept") && jump_count < max_jump:
+		jump_count += 1
+		velocity.y -= jump_force
+		velocity.x = input
+	if !is_on_floor() && Input.is_action_just_released("ui_accept") && jump_count < max_jump:
+		velocity.y = gravity
+		velocity.x = input
+
 	gravity_force()
 	move_and_slide()
 	
