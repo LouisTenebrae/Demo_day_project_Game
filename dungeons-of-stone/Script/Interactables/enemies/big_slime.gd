be7499e90fd86e25783f79977b887bc9334db2a7
Louis Martinez Echevarria <louismartinezechevarria@gmail.com>
2024-10-17 23:27:37 -0400

    Already discussed, it's late, just want to sleep

--- dungeons-of-stone/Script/Interactables/enemies/big_slime.gd
+++ dungeons-of-stone/Script/Interactables/enemies/big_slime.gd
@@ -6,10 +6,11 @@ extends CharacterBody2D
 var dir
 
 var current_state
-enum slime_states {IDLE, RIGHT, LEFT, DEAD}
+enum slime_states {IDLE, RIGHT,LEFT, CHASE, ATTACK, DEAD}
 
 func _ready() -> void:
-	random_generator()
+	$attack/CollisionShape2D.disabled = true
+	_on_timer_timeout()
 	
 	
 func _physics_process(_delta: float) -> void:
@@ -27,6 +28,8 @@ func _physics_process(_delta: float) -> void:
 			move_right()
 		slime_states.LEFT:
 			move_left()
+		slime_states.ATTACK:
+			attack()
 		slime_states.DEAD:
 			dead()
 					
@@ -42,13 +45,17 @@ func move_right():
 		velocity = Vector2.RIGHT * speed
 		$AnimationPlayer.play("walk")
 		$Sprite2D.flip_h = true
+		$attack/CollisionShape2D.position.x = 12.2
+		$aim/aim.position.x = 11
 
 func move_left():
 	if is_on_floor():
 		velocity = Vector2.LEFT * speed
 		$AnimationPlayer.play("walk")
 		$Sprite2D.flip_h = false
-		
+		$attack/CollisionShape2D.position.x = -12.2
+		$aim/aim.position.x = -11
+
 func dead():
 	$AnimationPlayer.play("dead")
 	await $AnimationPlayer.animation_finished
@@ -75,5 +82,32 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	random_generator()
-	$Timer.start
+	if  $LeftRayCast2D.is_colliding():
+		chase_left()
+	elif $RigthRayCast2D.is_colliding():
+		chase_right()
+	elif random_generator():
+		$Timer.start
+	
+	
+
+func chase_left():
+	if $LeftRayCast2D.is_colliding():
+		current_state = slime_states.LEFT
+
+func chase_right():
+	if $RigthRayCast2D.is_colliding():
+		current_state = slime_states.RIGHT
+
+
+
+func attack():
+	velocity.x = 0
+	$AnimationPlayer.play("attack")
+	await $AnimationPlayer.animation_finished
+	_on_timer_timeout()
+
+
+func _on_aim_body_entered(body: Node2D) -> void:
+	if body is Player:
+		current_state = slime_states.ATTACK
